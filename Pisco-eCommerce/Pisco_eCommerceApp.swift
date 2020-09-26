1fb039ba487fc3ffbac371cb43222f85809f893b
Victor Yucra <victorvsnet@gmail.com>
2020-09-26 01:01:28 -0500

    init project

---
+++ Pisco-eCommerce/Pisco_eCommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pisco_eCommerceApp.swift
+//  Pisco-eCommerce
+//
+//  Created by Victor Yucra on 9/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Pisco_eCommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
